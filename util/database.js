e675a0f243148ab5b28304c685eaa9195d297f36
maorm2 <maorminyan@gmail.com>
2021-03-28 22:47:15 +0300

    initial commit

---
+++ util/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'tastasalg', {
+    dialect: 'mysql',
+    host: 'localhost'
+});
+
+module.exports = sequelize; 
