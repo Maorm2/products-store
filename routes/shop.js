88030edc3391c715761eab22469414d14d4ab005
maorm2 <maorminyan@gmail.com>
2021-03-28 23:44:48 +0300

    Bug fixed

--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 
 const shopController = require('../controllers/shop');
-//const { route } = require('./admin');
 
 const router = express.Router();
 
